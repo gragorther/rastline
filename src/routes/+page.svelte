8150b48e138935315b7c37bc08c1a7fe0467d260
Gregor <gregorpodjed@yahoo.com>
2024-10-16 19:39:12 +0200

    Made the site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,50 @@
-<div class="grid justify-items-center gap-5">
-	<h1 class="bg-yellow-200 font-black text-2xl p-5 border-red-300 border-2 rounded-md text-center">
-		Hello World, Hello Grace!
-	</h1>
-	<img class="rounded-md w-96" src="art.webp" />
+<h1 class="text-5xl">Rastline</h1>
+<p>
+	Rastlíne (znanstveno ime Plantae) so eno izmed kraljestev živih bitij, v katerega uvrščamo
+	približno 300.000 danes znanih vrst. Skupina vključuje splošno razširjene in poznane življenjske
+	oblike, kot so drevesa, cvetnice, trave, praprotnice in mahovi, izključuje pa rdeče in rjave alge,
+	živali, glive, arheje ter bakterije. Za rastline je značilno, da razen izjem pridobivajo energijo
+	za rast in delovanje organizma iz sončne svetlobe s pomočjo procesa fotosinteze, ki poteka v
+	kloroplastih, poleg tega imajo njihove celice celične stene, grajene iz celuloze. Z rastlinami se
+	znanstveno ukvarja botanika, ki je panoga biologije. V preteklosti so med rastline uvrščali tudi
+	alge, glive in druge nesorodne avtotrofne organizme, to tradicijo botanika do neke mere ohranja še
+	danes. Skupnosti vseh rastlin na določenem območju v določenem času pravimo flora.
+</p>
+<div class="grid grid-cols-2 font-black text-2xl p-5 rounded-2xl">
+	<div
+		class="flex space-y-5 flex-col items-center font-black p-5 shadow-xl border-red-300 border-2 rounded-md bg-green-700 m-12 shadow-4xl"
+	>
+		<h1 class="text-4xl">Banana</h1>
+		<p class="text-sm">
+			Banana je užiten sadež, plod različnih vrst zelnatih rastlin iz roda Musa[1] in je lahko
+			različnih barv, velikosti in trdnosti. Botanično je jagoda.[2][3] Običajno je podolgovata in
+			ukrivljena, z mehkim škrobnatim mesom, pokritim z zelenim, rumenim, rdečim ali vijoličnim
+			olupkom, ki ob zrelosti porjavi. Sadeži v šopih visijo z drevesa. Skoraj vse moderne jedilne
+			partenokarpne (brez semen) banane izhajajo iz dveh divjih vrst – Musa acuminata in Musa
+			balbisiana. Banane so po pridelani količini na četrtem mestu, za pšenico, rižem in mlekom. Na
+			zahodnem tržišču so skoraj izključno sorte Cavendish. Ogroža jih glivična panamska bolezen.[4]
+			Edina članica Evropske unije, ki ima plantaže bananovcev, je Španija (Kanarski otoki).
+		</p>
+		<img src="Bananavarieties.jpg" class="rounded-br-lg" alt="banana varieties" />
+		<a
+			href="https://sl.wikipedia.org/wiki/Banana"
+			class="bg-yellow-400 text-white rounded-3xl hover:bg-yellow-800 px-20 shadow-xl">Več o tem</a
+		>
+	</div>
+	<div
+		class="flex flex-col space-y-5 items-center font-black p-5 shadow-xl border-red-300 border-2 rounded-md bg-green-700 m-12 shadow-4xl"
+	>
+		<h1 class="text-4xl">Jabolko</h1>
+		<p class="text-sm">
+			Jabolko [jábou̯ko] je sadež, ki raste na drevesu jablani. Spada med kulturne rastline, ki jih
+			je vzgojil človek s pomočjo križanja. Korenine jabolka izvirajo iz Azije. V Evropo je verjetno
+			prispelo s trgovci. Obstaja več kot 7500 kultivarjev jabolk. Jabolko zraste iz pet-listnega
+			cveta.
+		</p>
+		<img src="jabolko.jpg" alt="An apple" class="rounded-br-lg size-64" />
+		<a
+			href="https://sl.wikipedia.org/wiki/Jabolko"
+			class="bg-yellow-400 text-white rounded-3xl hover:bg-yellow-800 px-20 shadow-xl">Več o tem</a
+		>
+	</div>
 </div>
